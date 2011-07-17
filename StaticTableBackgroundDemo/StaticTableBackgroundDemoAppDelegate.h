8ac944e4a4e3fa8a4f9fe5acfae785b00aaeeb07
James Magahern <james@Hyperlion.local>
2011-07-17 03:04:19 -0700

    Initial Commit

---
+++ StaticTableBackgroundDemo/StaticTableBackgroundDemoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  StaticTableBackgroundDemoAppDelegate.h
+//  StaticTableBackgroundDemo
+//
+//  Created by James Magahern on 7/17/11.
+//  Copyright 2011 omegaHern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StaticTableBackgroundDemoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StaticTableBackgroundDemo/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  StaticTableBackgroundDemo
+//
+//  Created by James Magahern on 7/17/11.
+//  Copyright 2011 omegaHern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "StaticTableBackgroundDemoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    int retVal = 0;
+    @autoreleasepool {
+        retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([StaticTableBackgroundDemoAppDelegate class]));
+    }
+    return retVal;
+}
